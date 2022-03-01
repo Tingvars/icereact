c7c17b4f59c704b2e9b1dbbb95d0a25262476369
Thora Ingvars <t.ingvarsdottir@gmail.com>
2022-03-01 15:24:53 +0000

    added adjectives to logic, added option to select adjectives on initial screen (not fully functional)

--- src/StartNewGame.js
+++ src/StartNewGame.js
@@ -3,12 +3,13 @@ import React, { useState } from 'react';
 import GameSentence from './GameSentence';
 
 const gameSettings = {
-     turnCounter: 1, questionCounter: 0, gameTurns: 0, rightAnswers: 0
+     turnCounter: 1, questionCounter: 0, gameTurns: 0, rightAnswers: 0, useAdj: false
     };
 
 export default function StartNewGame() {
     const [hasClicked, setHasClicked] = useState(false);
     const [turnCount, setTurnCount] = useState(1);
+    const [useAdjectives, setUseAdjectives] = useState(false);
 
 function StartGame() {
 setHasClicked(true);
@@ -16,6 +17,7 @@ setHasClicked(true);
 
     if (hasClicked === true) {
         gameSettings.gameTurns = turnCount;
+        gameSettings.useAdj = useAdjectives;
         return <div>
             < GameSentence gameSettings={gameSettings}/>
         </div>
@@ -23,8 +25,17 @@ setHasClicked(true);
 
     return ( 
         <div className = "isSentence" >
-            < h2 > Welcome to the game! < /h2 >
+            <h2> Welcome to the game! </h2>
             <h3>You'll enter some settings on this page and then click Start.</h3>
+            <h4>Tick this box to select adjectives:</h4>
+            <input type="checkbox" name="adjectives" onChange={() => {
+                if (useAdjectives) {
+                    setUseAdjectives(false);
+                } else {
+                    setUseAdjectives(true);
+                }
+          }}
+            ></input>
             <h4>Please select the number of turns you want to play:</h4>
             <input
           type="number"
--- src/library.js
+++ src/library.js
@@ -13,8 +13,106 @@ const subjects = [
 ];
 
 const verbs = [
+//hitta, sakna, tala við, horfa á, hjálpa, 
+//sjá, eiga, muna eftir, hata, elska, 
+//leita að, drekka, borða, henda
 //grammar 0 = nom, 1 = acc, 2 = dat, 3 = gen
-
+{verbis:"hitta",
+verben:"meet",
+forms:[
+["hitti", "meet"], 
+["hittir", "meet"], 
+["hittir", "meets"], 
+["hittum", "meet"], 
+["hittið", "meet"], 
+["hitta", "meet"]
+],
+grammar:1
+},
+{verbis:"sakna",
+verben:"miss",
+forms:[
+["sakna", "miss"], 
+["saknar", "miss"], 
+["saknar", "misses"], 
+["söknum", "miss"], 
+["saknið", "miss"], 
+["sakna", "miss"]
+],
+grammar:3
+},
+{verbis:"tala við",
+verben:"talk to",
+forms:[
+["tala við", "talk to"], 
+["talar við", "talk to"], 
+["talar við", "talks to"], 
+["tölum við", "talk to"], 
+["talið við", "talk to"], 
+["tala við", "talk to"]
+],
+grammar:1
+},
+{verbis:"horfa á",
+verben:"watch",
+forms:[
+["horfi á", "watch"], 
+["horfir á", "watch"], 
+["horfir á", "watches"], 
+["horfum á", "watch"], 
+["horfið á", "watch"], 
+["horfa á", "watch"]
+],
+grammar:1
+},
+{verbis:"hjálpa",
+verben:"help",
+forms:[
+["hjálpa", "help"], 
+["hjálpar", "help"], 
+["hjálpar", "helps"], 
+["hjálpum", "help"], 
+["hjálpið", "help"], 
+["hjálpa", "help"]
+],
+grammar:2
+},
+{verbis:"sjá",
+verben:"see",
+forms:[
+["sé", "see"], 
+["sérð", "see"], 
+["sér", "sees"], 
+["sjáum", "see"], 
+["sjáið", "see"], 
+["sjá", "see"]
+],
+grammar:1
+},
+{verbis:"eiga",
+verben:"own",
+forms:[
+["á", "own"], 
+["átt", "own"], 
+["á", "owns"], 
+["eigum", "own"], 
+["eigið", "own"], 
+["eiga", "own"]
+],
+grammar:1
+},
+{verbis:"muna eftir",
+verben:"remember",
+forms:[
+["man eftir", "remember"], 
+["manst eftir", "remember"], 
+["man eftir", "remembers"], 
+["munum eftir", "remember"], 
+["munið eftir", "remember"], 
+["muna eftir", "remember"]
+],
+grammar:2
+},
 {verbis:"hata",
 verben:"hate",
 forms:[
@@ -90,8 +188,62 @@ grammar:2
 ];
 
 const objects = [
+//hundur, tölva, ljós, bolti, bíll, 
+//mynd, epli, hús, hestur, borð
+//genders: 0=masc, 1=fem, 2=neut
+{objis:"hundur",
+objen:"dog",
+gender:0,
+forms:[
+[["hundur", "a dog"], ["hund", "a dog"], ["hundi", "a dog"], ["hunds", "a dog"]], 
+[["hundar", "dogs"], ["hunda", "dogs"], ["hundum", "dogs"], ["hunda", "dogs"]],  
+[["hundurinn", "the dog"], ["hundinn", "the dog"], ["hundinum", "the dog"], ["hundsins", "the dog"]],  
+[["hundarnir", "the dogs"], ["hundana", "the dogs"], ["hundunum", "the dogs"], ["hundanna", "the dogs"]], 
+],
+},
+{objis:"tölva",
+objen:"computer",
+gender:1,
+forms:[
+[["tölva", "a computer"], ["tölvu", "a computer"], ["tölvu", "a computer"], ["tölvu", "a computer"]], 
+[["tölvur", "computers"], ["tölvur", "computers"], ["tölvum", "computers"], ["tölva", "computers"]],  
+[["tölvan", "the computer"], ["tölvuna", "the computer"], ["tölvunni", "the computer"], ["tölvunnar", "the computer"]],  
+[["tölvurnar", "the computers"], ["tölvurnar", "the computers"], ["tölvunum", "the computers"], ["tölvanna", "the computers"]], 
+],
+},
+{objis:"ljós",
+objen:"light",
+gender:2,
+forms:[
+[["ljós", "a light"], ["ljós", "a light"], ["ljósi", "a light"], ["ljóss", "a light"]], 
+[["ljós", "lights"], ["ljós", "lights"], ["ljósum", "lights"], ["ljósa", "lights"]],  
+[["ljósið", "the light"], ["ljósið", "the light"], ["ljósinu", "the light"], ["ljóssins", "the light"]],  
+[["ljósin", "the lights"], ["ljósin", "the lights"], ["ljósunum", "the lights"], ["ljósanna", "the lights"]], 
+],
+},
+{objis:"bolti",
+objen:"ball",
+gender:0,
+forms:[
+[["bolti", "a ball"], ["bolta", "a ball"], ["bolta", "a ball"], ["bolta", "a ball"]], 
+[["boltar", "balls"], ["bolta", "balls"], ["boltum", "balls"], ["bolta", "balls"]],  
+[["boltinn", "the ball"], ["boltann", "the ball"], ["boltanum", "the ball"], ["boltans", "the ball"]],  
+[["boltarnir", "the balls"], ["boltana", "the balls"], ["boltunum", "the balls"], ["boltanna", "the balls"]], 
+],
+},
+{objis:"bíll",
+objen:"car",
+gender:0,
+forms:[
+[["bíll", "a car"], ["bíl", "a car"], ["bíl", "a car"], ["bíls", "a car"]], 
+[["bílar", "cars"], ["bíla", "cars"], ["bílum", "cars"], ["bíla", "cars"]],  
+[["bíllinn", "the car"], ["bílinn", "the car"], ["bílnum", "the car"], ["bílsins", "the car"]],  
+[["bílarnir", "the cars"], ["bílana", "the cars"], ["bílunum", "the cars"], ["bílanna", "the cars"]], 
+],
+},
 {objis:"mynd",
 objen:"picture",
+gender:1,
 forms:[
 [["mynd", "a picture"], ["mynd", "a picture"], ["mynd", "a picture"], ["myndar", "a picture"]], 
 [["myndir", "pictures"], ["myndir", "pictures"], ["myndum", "pictures"], ["mynda", "pictures"]],  
@@ -101,6 +253,7 @@ forms:[
 },
 {objis:"epli",
 objen:"apple",
+gender:2,
 forms:[
 [["epli", "an apple"], ["epli", "an apple"], ["epli", "an apple"], ["eplis", "an apple"]], 
 [["epli", "apples"], ["epli", "apples"], ["eplum", "apples"], ["epla", "apples"]],  
@@ -110,6 +263,7 @@ forms:[
 },
 {objis:"hús",
 objen:"house",
+gender:2,
 forms:[
 [["hús", "a house"], ["hús", "a house"], ["húsi", "a house"], ["húss", "a house"]], 
 [["hús", "houses"], ["hús", "houses"], ["húsum", "houses"], ["húsa", "houses"]],  
@@ -119,6 +273,7 @@ forms:[
 },
   {objis:"hestur",
   objen:"horse",
+  gender:0,
   forms:[
   [["hestur", "a horse"], ["hest", "a horse"], ["hesti", "a horse"], ["hests", "a horse"]], 
   [["hestar", "horses"], ["hesta", "horses"], ["hestum", "horses"], ["hesta", "horses"]],  
@@ -128,6 +283,7 @@ forms:[
 },
 {objis:"borð",
   objen:"table",
+  gender:2,
   forms:[
     [["borð", "a table"], ["borð", "a table"], ["borði", "a table"], ["borðs", "a table"]], 
     [["borð", "tables"], ["borð", "tables"], ["borðum", "tables"], ["borða", "tables"]],  
@@ -137,11 +293,186 @@ forms:[
   },
 ]
 
+const adjectives = [
+  {adjis: "stór",
+  adjen: "big",
+  forms:[  
+    [//def
+    [//masc
+      [//sing
+        "stóri", "stóra", "stóra", "stóra"
+      ],
+      [//plur
+        "stóru", "stóru", "stóru", "stóru"
+      ]
+    ],
+    [//fem
+      [//sing
+        "stóra", "stóru", "stóru", "stóru"
+      ],
+      [//plur
+        "stóru", "stóru", "stóru", "stóru"
+      ]
+    ],
+    [//neut
+      [//sing
+        "stóra", "stóra", "stóra", "stóra"
+      ],
+      [//plur
+        "stóru", "stóru", "stóru", "stóru"
+      ]
+    ]
+  ],
+  [//indef
+    [//masc
+      [//sing
+        "stór", "stóran", "stórum", "stórs"
+      ],
+      [//plur
+        "stórir", "stóra", "stórum", "stórra"
+      ]
+    ],
+    [//fem
+      [//sing
+        "stór", "stóra", "stórri", "stórrar"
+      ],
+      [//plur
+        "stórar", "stórar", "stórum", "stórra"
+      ]
+    ],
+    [//neut
+      [//sing
+        "stórt", "stórt", "stóru", "stórs"
+      ],
+      [//plur
+        "stór", "stór", "stórum", "stórra"
+      ]
+    ]
+  ]
+]
+},
+{adjis: "gömul",
+  adjen: "old",
+  forms:[  
+    [//def
+    [//masc
+      [//sing
+        "gamli", "gamla", "gamla", "gamla"
+      ],
+      [//plur
+        "gömlu", "gömlu", "gömlu", "gömlu"
+      ]
+    ],
+    [//fem
+      [//sing
+        "gamla", "gömlu", "gömlu", "gömlu"
+      ],
+      [//plur
+        "gömlu", "gömlu", "gömlu", "gömlu"
+      ]
+    ],
+    [//neut
+      [//sing
+        "gamla", "gamla", "gamla", "gamla"
+      ],
+      [//plur
+        "gömlu", "gömlu", "gömlu", "gömlu"
+      ]
+    ]
+  ],
+  [//indef
+    [//masc
+      [//sing
+        "gamall", "gamlan", "gömlum", "gamals"
+      ],
+      [//plur
+        "gamlir", "gamla", "gömlum", "gamalla"
+      ]
+    ],
+    [//fem
+      [//sing
+        "gömul", "gamla", "gamalli", "gamallar"
+      ],
+      [//plur
+        "gamlar", "gamlar", "gömlum", "gamalla"
+      ]
+    ],
+    [//neut
+      [//sing
+        "gamalt", "gamalt", "gömlu", "gamals"
+      ],
+      [//plur
+        "gömul", "gömul", "gömlum", "gamalla"
+      ]
+    ]
+  ]
+]
+},
+{adjis: "góð",
+  adjen: "good",
+  forms:[  
+    [//def
+    [//masc
+      [//sing
+        "góði", "góða", "góða", "góða"
+      ],
+      [//plur
+        "góðu", "góðu", "góðu", "góðu"
+      ]
+    ],
+    [//fem
+      [//sing
+        "góða", "góðu", "góðu", "góðu"
+      ],
+      [//plur
+        "góðu", "góðu", "góðu", "góðu"
+      ]
+    ],
+    [//neut
+      [//sing
+        "góða", "góða", "góða", "góða"
+      ],
+      [//plur
+        "góðu", "góðu", "góðu", "góðu"
+      ]
+    ]
+  ],
+  [//indef
+    [//masc
+      [//sing
+        "góður", "góðan", "góðum", "góðs"
+      ],
+      [//plur
+        "góðir", "góða", "góðum", "góðra"
+      ]
+    ],
+    [//fem
+      [//sing
+        "góð", "góða", "góðri", "góðrar"
+      ],
+      [//plur
+        "góðar", "góðar", "góðum", "góðra"
+      ]
+    ],
+    [//neut
+      [//sing
+        "gott", "gott", "góðu", "góðs"
+      ],
+      [//plur
+        "góð", "góð", "góðum", "góðra"
+      ]
+    ]
+  ]
+]
+}
+]
+
 function getRandomFromList(list) {
   return list[Math.floor(Math.random() * list.length)]
 }
 
 export default function getSentence(randomFunc = getRandomFromList) {
+  let useAdj = true;
   let pickedSubject = randomFunc(subjects);
   let subjectGrammar = pickedSubject.grammar;
 
@@ -155,324 +486,60 @@ export default function getSentence(randomFunc = getRandomFromList) {
   let enSubject = pickedSubject.en;
   let isVerb = pickedVerb.forms[subjectGrammar][0];
   let enVerb = pickedVerb.forms[subjectGrammar][1];
-  let isObject = pickedForm[verbGrammar][0];
-  let enObject = pickedForm[verbGrammar][1];
+  let objectGender = pickedObj.gender;
+  let pickedAdjective = randomFunc(adjectives);
+  let enAdjective = pickedAdjective.adjen;
+  let adjDefOrIndef;
+  if (pickedForm < 2) {
+    adjDefOrIndef = 0;
+  } else {
+    adjDefOrIndef = 1;
+  }
+  let adjSingOrPlur;
+  if (pickedForm = 0) {
+    adjSingOrPlur = 0;
+  } else if (pickedForm = 1) {
+    adjSingOrPlur = 1;
+  } else if (pickedForm = 2) {
+    adjSingOrPlur = 0;
+  } else if (pickedForm = 3) {
+    adjSingOrPlur = 1;
+  }
+  let isAdjective = pickedAdjective.forms[adjDefOrIndef][objectGender][adjSingOrPlur][verbGrammar];
+  
+    //gender (nos 0,1,2) from object objectGender, 
+    //case (nos 0,1,2,3) from verb verbGrammar, 
+    //def&indef (0(sing),1(plur),2(sing),3(plur))from object pickedForm
+  
+  let isObject = pickedObj.forms[pickedForm][verbGrammar][0];
+  let enObject = pickedObj.forms[pickedForm][verbGrammar][1];
   let isSubjHeadword = pickedSubject.is;
   let isVerbHeadword = pickedVerb.verbis;
+  let isAdjectiveHeadword = pickedAdjective.adjis;
   let isObjHeadword = pickedObj.objis;
-  let enSentence = enSubject + " " + enVerb + " " + enObject;
+  let enSentence;
+  if (useAdj) {
+    if (adjDefOrIndef = 1) {
+    enSentence = enSubject + " " + enVerb + " " + enAdjective + " " + enObject;
+  } else if (adjDefOrIndef = 0) {
+    let newEnObject = enObject.slice(4);
+    enSentence = enSubject + " " + enVerb + " " + "the " + enAdjective + " " + newEnObject;
+  }
+  } else {
+    enSentence = enSubject + " " + enVerb + " " + enObject;
+  }
 
+  if (useAdj) {
+    return { 
+      fields: [isSubject, isVerb, isAdjective, isObject], 
+      english: enSentence, 
+      headwords: [isSubjHeadword, isVerbHeadword, isAdjectiveHeadword, isObjHeadword]
+    }
+  } else {
   return { 
     fields: [isSubject, isVerb, isObject], 
     english: enSentence, 
     headwords: [isSubjHeadword, isVerbHeadword, isObjHeadword]
   }
 }
-
-
-// const verbs = [
-
-//   sja,
-//   eiga,
-//   borda,
-//   maneftir,
-//   hjalpa,
-//   horfaa,
-//   talavid,
-//   hitta,
-//   henda,
-//   sakna
-// ];
-
-// const sja = new Verb([
-//   "sé",
-//   "sérð",
-//   "sér",
-//   "sjáum",
-//   "sjáið",
-//   "sjá",
-// ], [
-//   "see",
-//   "see",
-//   "sees",
-//   "see",
-//   "see",
-//   "see",
-// ], "acc");
-
-// const eiga = new Verb([
-//   "á",
-//   "átt",
-//   "á",
-//   "eigum",
-//   "eigið",
-//   "eiga",
-// ], [
-//   "own",
-//   "own",
-//   "owns",
-//   "own",
-//   "own",
-//   "own",
-// ], "acc");
-
-// const borda = new Verb([
-//   "borða",
-//   "borðar",
-//   "borðar",
-//   "borðum",
-//   "borðið",
-//   "borða",
-// ], [
-//   "eat",
-//   "eat",
-//   "eats",
-//   "eat",
-//   "eat",
-//   "eat",
-// ], "acc");
-
-// const maneftir = new Verb([
-//   "man eftir",
-//   "manst eftir",
-//   "man eftir",
-//   "munum eftir",
-//   "munið eftir",
-//   "muna eftir",
-// ], [
-//   "remember",
-//   "remember",
-//   "remembers",
-//   "remember",
-//   "remember",
-//   "remember",
-// ], "dat");
-
-// const hjalpa = new Verb([
-//   "hjálpa",
-//   "hjálpar",
-//   "hjálpar",
-//   "hjálpum",
-//   "hjálpið",
-//   "hjálpa",
-// ], [
-//   "help",
-//   "help",
-//   "helps",
-//   "help",
-//   "help",
-//   "help",
-// ], "dat");
-
-// const horfaa = new Verb([
-//   "horfi á",
-//   "horfir á",
-//   "horfir á",
-//   "horfum á",
-//   "horfið á",
-//   "horfa á",
-// ], [
-//   "watch",
-//   "watch",
-//   "watches",
-//   "watch",
-//   "watch",
-//   "watch",
-// ], "acc");
-
-// const talavid = new Verb([
-//   "tala við",
-//   "talar við",
-//   "talar við",
-//   "tölum við",
-//   "talið við",
-//   "tala við",
-// ], [
-//   "talk to",
-//   "talk to",
-//   "talks to",
-//   "talk to",
-//   "talk to",
-//   "talk to",
-// ], "acc");
-
-// const hitta = new Verb([
-//   "hitti",
-//   "hittir",
-//   "hittir",
-//   "hittum",
-//   "hittið",
-//   "hitta",
-// ], [
-//   "meet",
-//   "meet",
-//   "meets",
-//   "meet",
-//   "meet",
-//   "meet",
-// ], "acc");
-
-// const henda = new Verb([
-//   "hendi",
-//   "hendir",
-//   "hendir",
-//   "hendum",
-//   "hendið",
-//   "henda",
-// ], [
-//   "throw",
-//   "throw",
-//   "throws",
-//   "throw",
-//   "throw",
-//   "throw",
-// ], "dat");
-
-// const sakna = new Verb([
-//   "sakna",
-//   "saknar",
-//   "saknar",
-//   "söknum",
-//   "saknið",
-//   "sakna",
-// ], [
-//   "miss",
-//   "miss",
-//   "misses",
-//   "miss",
-//   "miss",
-//   "miss",
-// ], "gen");
-
-// class Object {
-// objList = [
-
-//     bill,
-//     bord,
-//     bolti,
-//     hestur,
-//     ljos,
-//     tolva,
-//     manneskja,
-//     hundur
-//   ];
-
-// const bill = [
-//   [
-//     ["bíll", "bíl", "bíl", "bíls"],
-//     ["bílar", "bíla", "bílum", "bíla"],
-//     ["bíllinn", "bílinn", "bílnum", "bílsins"],
-//     ["bílarnir", "bílana", "bílunum", "bílanna"],
-//   ],
-//   [
-//     ["a car", "a car", "a car", "a car"],
-//     ["cars", "cars", "cars", "cars"],
-//     ["the car", "the car", "the car", "the car"],
-//     ["the cars", "the cars", "the cars", "the cars"],
-//   ],
-// ];
-
-// const bord = [
-//   [
-//     ["borð", "borð", "borði", "borðs"],
-//     ["borð", "borð", "borðum", "borða"],
-//     ["borðið", "borðið", "borðinu", "borðsins"],
-//     ["borðin", "borðin", "borðunum", "borðanna"],
-//   ],
-//   [
-//     ["a table", "a table", "a table", "a table"],
-//     ["tables", "tables", "tables", "tables"],
-//     ["the table", "the table", "the table", "the table"],
-//     ["the tables", "the tables", "the tables", "the tables"],
-//   ],
-// ];
-
-// const bolti = [
-//   [
-//     ["bolti", "bolta", "bolta", "bolta"],
-//     ["boltar", "bolta", "boltum", "bolta"],
-//     ["boltinn", "boltann", "boltanum", "boltans"],
-//     ["boltarnir", "boltana", "boltunum", "boltanna"],
-//   ],
-//   [
-//     ["a ball", "a ball", "a ball", "a ball"],
-//     ["balls", "balls", "balls", "balls"],
-//     ["the ball", "the ball", "the ball", "the ball"],
-//     ["the balls", "the balls", "the balls", "the balls"],
-//   ],
-// ];
-
-// const hestur = [
-//   [
-//     ["hestur", "hest", "hesti", "hests"],
-//     ["hestar", "hesta", "hestum", "hesta"],
-//     ["hesturinn", "hestinn", "hestinum", "hestsins"],
-//     ["hestarnir", "hestana", "hestunum", "hestanna"],
-//   ],
-//   [
-//     ["a horse", "a horse", "a horse", "a horse"],
-//     ["horses", "horses", "horses", "horses"],
-//     ["the horse", "the horse", "the horse", "the horse"],
-//     ["the horses", "the horses", "the horses", "the horses"],
-//   ],
-// ];
-
-// const ljos = [
-//   [
-//     ["ljós", "ljós", "ljósi", "ljóss"],
-//     ["ljós", "ljós", "ljósum", "ljósa"],
-//     ["ljósið", "ljósið", "ljósinu", "ljóssins"],
-//     ["ljósin", "ljósin", "ljósunum", "ljósanna"],
-//   ],
-//   [
-//     ["a light", "a light", "a light", "a light"],
-//     ["lights", "lights", "lights", "lights"],
-//     ["the light", "the light", "the light", "the light"],
-//     ["the lights", "the lights", "the lights", "the lights"],
-//   ],
-// ];
-
-// const tolva = [
-//   [
-//     ["tölva", "tölvu", "tölvu", "tölvu"],
-//     ["tölvur", "tölvur", "tölvum", "tölva"],
-//     ["tölvan", "tölvuna", "tölvunni", "tölvunnar"],
-//     ["tölvurnar", "tölvurnar", "tölvunum", "tölvanna"],
-//   ],
-//   [
-//     ["a computer", "a computer", "a computer", "a computer"],
-//     ["computers", "computers", "computers", "computers"],
-//     ["the computer", "the computer", "the computer", "the computer"],
-//     ["the computers", "the computers", "the computers", "the computers"],
-//   ],
-// ];
-
-// const manneskja = [
-//   [
-//     ["manneskja", "manneskju","manneskju", "manneskju"],
-//     ["manneskjur", "manneskjur", "manneskjum", "manneskja"],
-//     ["manneskjan", "manneskjuna", "manneskjunni", "manneskjunnar"],
-//     ["manneskjurnar", "manneskjurnar", "manneskjunum", "manneskjanna"],
-//   ],
-//   [
-//     ["a person", "a person", "a person", "a person"],
-//     ["people", "people", "people", "people"],
-//     ["the person", "the person", "the person", "the person"],
-//     ["the people", "the people", "the people", "the people"],
-//   ],
-// ];
-
-// const hundur = [
-//   [
-//     ["hundur", "hund","hundi", "hunds"],
-//     ["hundar", "hunda", "hundum", "hunda"],
-//     ["hundurinn", "hundinn", "hundinum", "hundsins"],
-//     ["hundarnir", "hundana", "hundunum", "hundanna"],
-//   ],
-//   [
-//     ["a dog", "a dog", "a dog", "a dog"],
-//     ["dogs", "dogs", "dogs", "dogs"],
-//     ["the dog", "the dog", "the dog", "the dog"],
-//     ["the dogs", "the dogs", "the dogs", "the dogs"],
-//   ],
-// ];
+}
